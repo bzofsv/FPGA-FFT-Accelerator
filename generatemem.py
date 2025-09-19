8fe3bb54d7e9cd73239a7347018ed77b9207f840
--replace-all <bzofsv@gmail.com>
2025-09-19 13:43:20 -0500

    added file to generate data samples, and twiddle factors

--- generatemem.py
+++ generatemem.py
@@ -1,4 +1,3 @@
-# run this with your fft_golden.py in the same folder
 from fft_golden import sine_q15, twiddles_q15, to_q15
 
 def write_hex(path, vals):
@@ -8,10 +7,10 @@ def write_hex(path, vals):
             f.write(f"{v:04x}\n")
 
 N = 256
-x = sine_q15(N, k=5, amp=0.9)     # real cosine, imag=0
+x = sine_q15(N, k=5, amp=0.9)    
 write_hex("FPGA FFT/data/stim256_re.mem", [r for (r,i) in x])
 write_hex("FPGA FFT/data/stim256_im.mem", [i for (r,i) in x])
 
-tw = twiddles_q15(N)              # length 128
+tw = twiddles_q15(N)           
 write_hex("FPGA FFT/data/tw_wr.mem", [wr for (wr,wi) in tw[:128]])
 write_hex("FPGA FFT/data/tw_wi.mem", [wi for (wr,wi) in tw[:128]])
